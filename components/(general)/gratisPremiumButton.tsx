1508a58aae4d3d51ba9fbb6fc8c90f911fcbd0be
Felix Manemann <felix.manemann@uni-hannover.de>
2026-05-16 10:25:02 +0200

    Fix:

--- components/(general)/gratisPremiumButton.tsx
+++ components/(general)/gratisPremiumButton.tsx
@@ -6,13 +6,11 @@ const GratisPremiumButton = ({
   aditionalStyles,
   handlePress,
   active,
-  disabled,
 }: {
   children?: React.ReactNode;
   aditionalStyles?: string;
   handlePress?: () => void;
   active?: boolean;
-  disabled?: boolean;
 }) => {
   const [isPressed, setIsPressed] = useState(false);
   return (
@@ -20,11 +18,11 @@ const GratisPremiumButton = ({
       activeOpacity={1}
       onPressIn={() => setIsPressed(true)}
       onPressOut={() => setIsPressed(false)}
-      disabled={disabled ?? (active ? !active : false)}
+      disabled={active ? !active : false}
       onPress={() => {
         handlePress ? handlePress() : null;
       }}
-      className="items-center justify-center rounded-full "
+      className="items-center justify-center rounded-full h-[30px] "
       style={{
         transform: [{ translateY: isPressed ? 2 : 0 }],
         shadowColor: "#1e3a8a",
